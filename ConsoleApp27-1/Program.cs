7ad10f59d3ae2a8da2ce9c509f8a3f3181ccb49f
muxazzXJohny <maracoco334@gmail.com>
2025-03-03 16:20:21 +0200

    Add files via upload

---
+++ ConsoleApp27-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.Write($"Введите элемент {i + 1}: ");
+                omas[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = omas.Length; i >= 0; i--)
+            {
+                Console.Write(omas[i] + " ");
+            }
+           
+            Console.ReadKey();
+        }
+        static public void Outputreverse(int[] array)
+        {
+            int[] reversedArray = (int[])array.Clone();
+            Array.Reverse(reversedArray);
+
+            foreach (int element in reversedArray)
+            {
+                Console.Write(element + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
